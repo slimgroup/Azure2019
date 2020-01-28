cad5c00a93fb900ed5b065486d444edadf4edd2a
Philipp Witte <p.witte@ymail.com>
2020-01-28 13:27:46 -0500

    change print to info

--- scripts/overthrust_3D_modeling.py
+++ scripts/overthrust_3D_modeling.py
@@ -1,7 +1,6 @@
 # Base imports
 import sys, os
 sys.path.insert(0, '/app/tti')
-
 from argparse import ArgumentParser
 import numpy as np
 import time
@@ -24,7 +23,6 @@ from utils import butter_bandpass_filter, butter_lowpass_filter
 # Azure utilities
 from AzureUtilities import read_h5_model, write_h5_model, process_summaries, read_coordinates
 
-
 def timer(start, message):
     end = time.time()
     hours, rem = divmod(end-start, 3600)
@@ -32,37 +30,33 @@ def timer(start, message):
     info('{}: {:d}:{:02d}:{:02d}'.format(message, int(hours), int(minutes), int(seconds)))
 
 #######################################################################################
+
 # Time resampling for shot records
 def resample(rec, num):
     start, stop = rec._time_range.start, rec._time_range.stop
     dt0 = rec._time_range.step
-
     new_time_range = TimeAxis(start=start, stop=stop, num=num)
     dt = new_time_range.step
-
     to_interp = np.asarray(rec.data)
     data = np.zeros((num, to_interp.shape[1]))
-
     for i in range(to_interp.shape[1]):
         tck = interpolate.splrep(rec._time_range.time_values, to_interp[:, i], k=3)
         data[:, i] = interpolate.splev(new_time_range.time_values, tck)
-
     coords_loc = np.asarray(rec.coordinates.data)
     # Return new object
     return data, coords_loc
 
-
 #######################################################################################
+
 # Segy writer for shot records
 def segy_write(data, sourceX, sourceZ, groupX, groupZ, dt, filename, sourceY=None,
                groupY=None, elevScalar=-1000, coordScalar=-1000):
-
     nt = data.shape[0]
     nxrec = len(groupX)
     if sourceY is None and groupY is None:
         sourceY = np.zeros(1, dtype='int')
         groupY = np.zeros(nxrec, dtype='int')
-
+    
     # Create spec object
     spec = so.spec()
     spec.ilines = np.arange(nxrec)    # dummy trace count
@@ -70,7 +64,6 @@ def segy_write(data, sourceX, sourceZ, groupX, groupZ, dt, filename, sourceY=Non
     spec.samples = range(nt)
     spec.format=1
     spec.sorting=1
-
     with so.create(filename, spec) as segyfile:
         for i in range(nxrec):
             segyfile.header[i] = {
@@ -92,6 +85,7 @@ def segy_write(data, sourceX, sourceZ, groupX, groupZ, dt, filename, sourceY=Non
         segyfile.dt=int(dt*1e3)
 
 #######################################################################################
+
 t0 = time.time()
 
 ####### Filter arguments
@@ -115,15 +109,14 @@ recloc = args.recloc
 modelloc = args.modelloc
 geomloc = args.geomloc
 freesurf = args.freesurf
-
 # Some parameters
 space_order = 12
 nbpml = 40
-
 timer(t0, 'Args process')
 t0 = time.time()
 
 #########################################################################################
+
 # Read models
 rho = read_h5_model(modelloc + 'rho_with_salt.h5')
 epsilon = read_h5_model(modelloc + 'epsilon_with_salt.h5')
@@ -131,30 +124,27 @@ delta = read_h5_model(modelloc + 'delta_with_salt.h5')
 theta = read_h5_model(modelloc + 'theta_with_salt.h5')
 phi = read_h5_model(modelloc + 'phi_with_salt.h5')
 vp = read_h5_model(modelloc + 'vp_fine_with_salt.h5')
-
 shape = (801, 801, 267)
 origin = (0.0, 0.0, 0.0)
 spacing = (12.5, 12.5, 12.5)
-
 model = Model(shape=shape, origin=origin, spacing=spacing, vp=vp, space_order=space_order,
               epsilon=epsilon, delta=delta, theta=theta, phi=phi, rho=rho, nbpml=nbpml)
-
 #######################################################################################
+
 # Get MPI info
 comm = model.grid.distributor.comm
 rank =  comm.Get_rank()
 size =  comm.size
-
 info("Min value in vp is %s " % (np.min(model.vp.data[:])))
 timer(t0, 'Read segy models')
 t0 = time.time()
 
 #########################################################################################
 # Model a 2D shot
+
 # Time axis
 tstart = 0.
-tn = 500.
-
+tn = 5000.
 dt = model.critical_dt
 f0 = 0.025
 time_axis = TimeAxis(start=tstart, step=dt, stop=tn)
@@ -167,11 +157,11 @@ file_rec = geomloc + 'rec_coordinates.h5'
 xsrc, ysrc, zsrc = read_coordinates(file_src)
 xrec, yrec, zrec = read_coordinates(file_rec)
 xsrc = xsrc[shot_id]; ysrc = ysrc[shot_id]; zsrc = zsrc[shot_id]
-print('Source location: ', xsrc, ', ', ysrc, ', ', zsrc)
+info('Source location: (%s, %s, %s)' % (xsrc, ysrc, zsrc))
 
 # Set up receiver coordinates
 rec_coords = np.concatenate((xrec.reshape(-1,1), yrec.reshape(-1,1), zrec.reshape(-1,1)), axis=1)
-print('Number of receivers: ', rec_coords.shape[0])
+info('Number of receivers: %s ' % rec_coords.shape[0])
 
 # Set up source coordinates
 src_coordinates = np.array([xsrc, ysrc, zsrc])
@@ -190,61 +180,56 @@ q_custom[:len(tnew), 0] = fq(tnew)
 q_custom[:, 0] = butter_bandpass_filter(q_custom[:, 0], .005, .030, 1/dt)
 q_custom[:, 0] = 1e1 * q_custom[:, 0] / np.max(q_custom[:, 0])
 src.data[:, 0] = q_custom[:, 0]
-
 timer(t0, 'Setup geometry')
 t0 = time.time()
 
 #########################################################################################
+
 # Propagator
 tti = TTIPropagators(model, space_order=space_order)
 
 #########################################################################################
+
 # Data
 info("Starting forward modeling")
-d_obs, u, v = tti.forward(src, rec_coords, freesurface=freesurf, autotune=('aggressive', 'runtime'))
-
+d_obs, u, v = tti.forward(src, rec_coords, freesurface=freesurf, autotune=('aggressive', 'runtime'), time_M=10)
 timer(t0, 'Run forward')
 t0 = time.time()
 
-
 #######################################################################################
-# Check output
 
+# Check output
 info("Nan values : %s" % np.any(np.isnan(d_obs.data[:])))
 info("saving shot records %srecloc%s%s" % (recloc, rank, shot_id))
-
 data_loc, coord_loc = resample(d_obs, 5001)
 np.save("%srecloc%s%s.npy"% (recloc, rank, shot_id), data_loc)
 np.save("%scoordloc%s%s.npy"% (recloc, rank, shot_id), coord_loc)
-
 timer(t0, 'Saved data')
 
 #######################################################################################
+
 # Merge output
-
 if rank == 0:
-
     data = np.load("%srecloc0%s.npy" % (recloc, shot_id))
     coords = np.load("%scoordloc0%s.npy" % (recloc, shot_id))
-
     for i in range(1, size):
+        
         # Wait until all files are ready
         while True:
             try:
                 datai = np.load("%srecloc%s%s.npy" %  (recloc, i, shot_id))
                 coordsi = np.load("%scoordloc%s%s.npy" %  (recloc, i, shot_id))
             except FileNotFoundError:
-                print("File not ready")
+                info("File not ready")
                 time.sleep(20)
-                print("Waited a bit trying again")
+                info("Waited a bit trying again")
             else:
-                print("File for rank %s found" % i)
+                info("File for rank %s found" % i)
                 break
         for j, c in enumerate(coordsi):
             if c not in coords:
                 np.vstack((coords, c))
                 np.hstack((data, datai[:, j]))
-
         os.system("rm -f %srecloc%s%s.npy %scoordloc%s%s.npy" % (recloc, i, shot_id, recloc, i, shot_id))
     info("Writing shot record of size (%s, %s) to segy file, maximum value is %s" % (data.shape[0], data.shape[1], np.max(data)))
     os.system("rm -f %srecloc%s%s.npy %scoordloc%s%s.npy" % (recloc, 0, shot_id, recloc, 0, shot_id))
@@ -256,4 +241,3 @@ if rank == 0:
                2.0,  "%srec%s.segy" % (recloc, shot_id),
                sourceY=[src.coordinates.data[0, 1]],
                groupY=coords[:, 1])
-
