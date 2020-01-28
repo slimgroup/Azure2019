a97885b6923d9512f25cadf8e4245edc6d2c4393
Philipp Witte <p.witte@ymail.com>
2020-01-28 17:50:26 -0500

    modeling w/o gathering

--- scripts/overthrust_3D_modeling_split.py
+++ scripts/overthrust_3D_modeling_split.py
@@ -1,159 +1,154 @@
-# Base imports
+import numpy as np
 import sys, os
 sys.path.insert(0, '/app/tti')
-from argparse import ArgumentParser
-import numpy as np
-import time
-
-# Devito imports
-from devito.logger import info  
-
-# tti imports from docker image
-from model import *
-from sources import *
-from tti_propagators import *
-
-# segy
+from model import Model
+from sources import RickerSource, TimeAxis, Receiver
+from tti_propagators import TTIPropagators
 import segyio as so
+from scipy import interpolate, ndimage
+from AzureUtilities import read_h5_model, write_h5_model, butter_bandpass_filter, butter_lowpass_filter, resample, process_summaries, read_coordinates, save_rec, restrict_model_to_receiver_grid, extent_gradient
+from scipy import interpolate, ndimage
+from mpi4py import MPI
+from devito import configuration
+configuration['mpi'] = True
 
-# Interpolation and filtering utils
-from scipy import interpolate
-from utils import butter_bandpass_filter, butter_lowpass_filter
+##########################################################################################
 
-# Azure utilities
-from AzureUtilities import read_h5_model, write_h5_model, process_summaries, read_coordinates, save_rec
+def limit_receiver_grid(xsrc, ysrc, xrec, yrec, zrec, maxoffset):
 
-def timer(start, message):
-    end = time.time()
-    hours, rem = divmod(end-start, 3600)
-    minutes, seconds = divmod(rem, 60)
-    info('{}: {:d}:{:02d}:{:02d}'.format(message, int(hours), int(minutes), int(seconds)))
+    xmin = np.max([xsrc - maxoffset, 12.5])
+    xmax = np.min([xsrc + maxoffset, 9987.5])
+    ymin = np.max([ysrc - maxoffset, 12.5])
+    ymax = np.min([ysrc + maxoffset, 9987.5])
 
+    print('xrange: ', xmin, ' to ', xmax)
+    print('yrange: ', ymin, ' to ', ymax)
 
-#######################################################################################
+    xnew = []
+    ynew = []
+    znew = []
 
-t0 = time.time()
+    for j in range(len(xrec)):
+        if xrec[j] >= xmin and xrec[j] <= xmax and yrec[j] >= ymin and yrec[j] <= ymax:
+            xnew.append(xrec[j])
+            ynew.append(yrec[j])
+            znew.append(zrec[j])
 
-####### Filter arguments
-description = ("3D modeling on tti overdone")
-parser = ArgumentParser(description=description)
-parser.add_argument("--id",  dest='shot_id', default=1, type=int,
-                    help="Shot number")
-parser.add_argument("--recloc",  dest='recloc', default="", type=str,
-                    help="Path to results directory in blob")
-parser.add_argument("--modelloc",  dest='modelloc', default="", type=str,
-                    help="Path to model directory in blob")
-parser.add_argument("--geomloc",  dest='geomloc', default="", type=str,
-                    help="Path to geometry directory in blob")
-parser.add_argument("--fs",  dest='freesurf', default=False, action='store_true',
-                    help="Freesurface")
-args = parser.parse_args()
+    xnew = np.array(xnew)
+    ynew = np.array(ynew)
+    znew = np.array(znew)
 
-# Get inputs
-shot_id = args.shot_id
-recloc = args.recloc
-modelloc = args.modelloc
-geomloc = args.geomloc
-freesurf = args.freesurf
-# Some parameters
-space_order = 12
-nbpml = 40
-timer(t0, 'Args process')
-t0 = time.time()
+    return xnew, ynew, znew
+
+def check_thomsen_parameters(model):
+    err = np.where(model.delta.data > model.epsilon.data)
+    num_err = np.sum([len(err[0]), len(err[1]), len(err[2])])
+    if num_err > 0:
+        print('Warning: delta > epsilon')
+
+##########################################################################################
+
+# Get runtime arguments and environment variables
+if len(sys.argv) > 1:
+    run_id = int(str(sys.argv[1]))
+else:
+    run_id = 0
+
+logging = os.environ["DEVITO_LOGGING"]
+num_threads = int(os.environ["OMP_NUM_THREADS"])
+rootpath = os.environ["AZ_BATCH_NODE_SHARED_DIR"]
+
+container = os.environ["BLOB_CONTAINER"]    # overthrust
+space_order = int(os.environ["SPACE_ORDER"])
+batchsize = int(os.environ["BATCHSIZE"])
 
 #########################################################################################
 
 # Read models
-rho = read_h5_model(modelloc + 'rho_with_salt.h5')
-epsilon = read_h5_model(modelloc + 'epsilon_with_salt.h5')
-delta = read_h5_model(modelloc + 'delta_with_salt.h5')
-theta = read_h5_model(modelloc + 'theta_with_salt.h5')
-phi = read_h5_model(modelloc + 'phi_with_salt.h5')
-vp = read_h5_model(modelloc + 'vp_fine_with_salt.h5')
-shape = (801, 801, 267)
-origin = (0.0, 0.0, 0.0)
-spacing = (12.5, 12.5, 12.5)
-model = Model(shape=shape, origin=origin, spacing=spacing, vp=vp, space_order=space_order,
-              epsilon=epsilon, delta=delta, theta=theta, phi=phi, rho=rho, nbpml=nbpml)
-#######################################################################################
+rho = read_h5_model(rootpath + '/overthrust/models/rho_with_salt.h5')
+epsilon = read_h5_model(rootpath + '/overthrust/models/epsilon_with_salt.h5')
+delta = read_h5_model(rootpath + '/overthrust/models/delta_with_salt.h5')
+theta = read_h5_model(rootpath + '/overthrust/models/theta_with_salt.h5')
+phi = read_h5_model(rootpath + '/overthrust/models/phi_with_salt.h5')
+m0 = read_h5_model(rootpath + '/overthrust/models/migration_velocity.h5')
+dm = read_h5_model(rootpath + '/overthrust/models/perturbation.h5')
+dm[:, :, 0:29] = 0.0    # Extend water column
+delta[delta >= epsilon] = .9 * epsilon[delta >= epsilon]    # fix thomsen parameters
 
-# Get MPI info
+# Read geometry
+source_indices = np.load(rootpath + '/overthrust/geometry/source_indices.npy', allow_pickle=True)
+idx = source_indices[run_id]
+print('Process source no. ', idx, ' out of ', batchsize)
+
+shape_full = (801, 801, 267)
+origin_full = (0.0, 0.0, 0.0)
+spacing = (12.5, 12.5, 12.5)
+
+# Read coordinates and source index
+file_src = rootpath + '/overthrust/geometry/src_coordinates.h5'
+file_rec = rootpath + '/overthrust/geometry/rec_coordinates.h5'
+xsrc, ysrc, zsrc = read_coordinates(file_src)
+xrec, yrec, zrec = read_coordinates(file_rec)
+xsrc = xsrc[idx]; ysrc = ysrc[idx]; zsrc = zsrc[idx]
+print('Source location: ', xsrc, ', ', ysrc, ', ', zsrc)
+
+# Limit receiver grid
+buffersize = 500    # in m
+maxoffset = 3787.5  # x/y direction in m
+xrec, yrec, zrec = limit_receiver_grid(xsrc, ysrc, xrec, yrec, zrec, maxoffset)
+
+# Restrict models to receiver grid
+print('Original shape: ', shape_full, ' and origin: ', origin_full)
+m0, shape, origin = restrict_model_to_receiver_grid(xsrc, xrec, m0, spacing, origin_full, buffer_size=buffersize, sy=ysrc, gy=yrec)
+rho = restrict_model_to_receiver_grid(xsrc, xrec, rho, spacing, origin_full, buffer_size=buffersize, sy=ysrc, gy=yrec)[0]
+epsilon = restrict_model_to_receiver_grid(xsrc, xrec, epsilon, spacing, origin_full, buffer_size=buffersize, sy=ysrc, gy=yrec)[0]
+delta = restrict_model_to_receiver_grid(xsrc, xrec, delta, spacing, origin_full, buffer_size=buffersize, sy=ysrc, gy=yrec)[0]
+theta = restrict_model_to_receiver_grid(xsrc, xrec, theta, spacing, origin_full, buffer_size=buffersize, sy=ysrc, gy=yrec)[0]
+phi = restrict_model_to_receiver_grid(xsrc, xrec, phi, spacing, origin_full, buffer_size=buffersize, sy=ysrc, gy=yrec)[0]
+dm = restrict_model_to_receiver_grid(xsrc, xrec, dm, spacing, origin_full, buffer_size=buffersize, sy=ysrc, gy=yrec)[0]
+print('New shape: ', shape, ' and origin ', origin)
+
+model = Model(shape=shape, origin=origin, spacing=spacing, vp=np.sqrt(1/m0), space_order=space_order,
+              epsilon=epsilon, delta=delta, theta=theta, phi=phi, rho=rho, nbpml=40, dm=dm, dt=0.64)
+
+check_thomsen_parameters(model)
+
 comm = model.grid.distributor.comm
-rank =  comm.Get_rank()
-size =  comm.size
-info("Min value in vp is %s " % (np.min(model.vp.data[:])))
-timer(t0, 'Read segy models')
-t0 = time.time()
+size = comm.Get_size()
+rank = comm.Get_rank()
 
 #########################################################################################
-# Model a 2D shot
 
 # Time axis
-tstart = 0.
+t0 = 0.
 tn = 500.
-dt = model.critical_dt
-f0 = 0.025
-time_axis = TimeAxis(start=tstart, step=dt, stop=tn)
+dt_shot = 4.
+nt = int(tn/dt_shot + 1)
+dt = 0.64   #model.critical_dt*.8
+time = TimeAxis(start=t0, step=dt, stop=tn)
 
 #########################################################################################
 
-# Source and receiver geometries
-nrecx = 101
-nrecy = 401
-nrec= nrecx * nrecy
 
-src_coords = np.empty((1, len(spacing)))
-src_coords[0, :] = np.array(model.domain_size) * .5
-src_coords[0, -1] = 287.5
+rec_coords = np.concatenate((xrec.reshape(-1,1), yrec.reshape(-1,1), zrec.reshape(-1,1)), axis=1)
+print('Number of receivers: ', rec_coords.shape[0])
 
-rec_coordinates = np.empty((nrecx, nrecy, 3))
-for i in range(nrecx):
-    for j in range(nrecy):
-        rec_coordinates[i, j, 0] = (i + 1) * spacing[0]
-        rec_coordinates[i, j, 1] = (j + 1) * spacing[1]
-        rec_coordinates[i, j, 2] = 6.0
-
-rec_coordinates = np.reshape(rec_coordinates, (nrec, 3))
-
-
-src = RickerSource(name='src', grid=model.grid, f0=f0, time_range=time_axis, npoint=1)
-src.coordinates.data[0, :] = src_coords[:]
-
-wavelet = np.concatenate((np.load("%swavelet.npy"%geomloc), np.zeros((100,))))
-twave = [i*1.2 for i in range(wavelet.shape[0])]
-tnew = [i*dt for i in range(int(1 + (twave[-1]-tstart) / dt))]
-fq = interpolate.interp1d(twave, wavelet, kind='linear')
-q_custom = np.zeros((time_axis.num, 1))
-q_custom[:len(tnew), 0] = fq(tnew)
-q_custom[:, 0] = butter_bandpass_filter(q_custom[:, 0], .005, .030, 1/dt)
-q_custom[:, 0] = 1e1 * q_custom[:, 0] / np.max(q_custom[:, 0])
-src.data[:, 0] = q_custom[:, 0]
-
-timer(t0, 'Setup geometry')
-t0 = time.time()
+# Source coordinates
+src_coordinates = np.array([xsrc, ysrc, zsrc])
+src = RickerSource(name='src', grid=model.grid, f0=0.02, time_range=time, npoint=1)
+src.coordinates.data[0, 0] = src_coordinates[0]
+src.coordinates.data[0, 1] = src_coordinates[1]
+src.coordinates.data[0, 2] = src_coordinates[2]
 
 
 #########################################################################################
 
-# Propagator
+# Devito operator
 tti = TTIPropagators(model, space_order=space_order)
 
-#########################################################################################
-
-# Data
-info("Starting forward modeling")
-d_obs, u, v = tti.forward(src, rec_coordinates, autotune=('aggressive', 'runtime'))
-timer(t0, 'Run forward')
-t0 = time.time()
-
-#######################################################################################
-
-# Check output
-info("Nan values : %s" % np.any(np.isnan(d_obs.data[:])))
-info("saving shot records %srecloc%s%s" % (recloc, rank, shot_id))
-nt_new = 126
+# Data and RTM
+d_obs, u0, v0, summary1 = tti.forward(src, rec_coords, autotune=('aggressive', 'runtime'))
 
 # Save shot
-filename = recloc + 'overthrust_3D_shot_no_' + str(shot_id)
-save_rec(d_obs, src_coords.reshape(3), filename, nt_new)
-timer(t0, 'Saved data')
+filename = rootpath + '/overthrust/data/overthrust_3D_data_source_' + str(idx)
+save_rec(d_obs, src_coordinates, filename, nt)
