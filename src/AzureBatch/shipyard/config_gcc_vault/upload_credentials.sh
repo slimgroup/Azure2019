7dd6a0eba8727edc2a8f85b11e6a3c453efa4515
Philipp Witte <p.witte@ymail.com>
2020-03-12 16:24:42 -0400

    add setup for HB nodes

--- src/AzureBatch/shipyard/config_gcc_vault/upload_credentials.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Store credentials.yaml in keyvault as mycredentials
-./shipyard keyvault add mycredentials \
---keyvault-uri <value> \
---aad-directory-id <value> \
---aad-application-id <value> \
---aad-auth-key <value> \
---credentials credentials.yaml
